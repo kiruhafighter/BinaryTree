6c027f9ec42153096f0d1c33032140acbfea7c49
Kyrylo Salomatin <kirill.salomatin@itperspectives.ro>
2023-01-27 17:02:17 +0200

    Add project files.

---
+++ BinarySearchTree.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    public class BinarySearchTree
+    {
+        public TreeNode Root { get; set; } = null;
+
+        public void Insert(int key, string value)
+        {
+            Root = InsertItem(Root, key, value);
+        }
+
+        public TreeNode InsertItem(TreeNode node, int key, string value)
+        {
+            TreeNode newNode = new TreeNode(key, value);
+            //If this is the first time you insert, create the root
+            if(node == null)
+            {
+                node = newNode;
+                return node;
+            }
+
+            //If this isn't the first insert, traverse, find null, insert
+            if(key < node.Key)
+            {
+                node.LeftChild = InsertItem(node.LeftChild, key, value);
+            }
+            else
+            {
+                node.RightChild = InsertItem(node.RightChild, key, value);
+            }
+            return node;
+        }
+
+        public string Find(int key)
+        {
+            TreeNode node = Find(Root, key);
+            return node == null ? null : node.Value;
+        }
+
+        private TreeNode? Find(TreeNode node, int key)
+        {
+            if(node == null || node.Key == key)
+            {
+                return node;
+            }
+            else if(key < node.Key)
+            {
+                return Find(node.LeftChild, key);
+            }
+            else if(key > node.Key)
+            {
+                return Find(node.RightChild, key);
+            }
+            return null;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using BinaryTree;
+
+BinarySearchTree bst = new BinarySearchTree();
+bst.Insert(7, "Dragonite");
+bst.Insert(23, "Behemos");
+bst.Insert(151, "Aotax");
+bst.Insert(4, "Ventus Viperagon");
+bst.Insert(1, "Dark Nemesious");  
